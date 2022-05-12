bb94050aff633c3e3b5baec4c6cbd73edc41bcfe
Anass Chbani <a.chbani@piman-group.fr>
2022-05-12 16:13:37 +0100

    Fix design du page login Fix design de forget password Ajout de fontawsome package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl : 'https://xxx/api/',
+  apiUrl : 'http://sirh-piman.dev.piman2-0.fr/api/public/api/',
   appBaseHref : './',
   logo: 'logo-pds.png',
   favicon: 'favicon.ico',
   title: 'PIMAN Group Solutions',
   backgroudImage: 'background.jpg',
-  photosBaseUrl: 'https://xxx/api/images/',
-  publicPhotosBaseUrl: 'https://xxx/api/public/storage/',
+  photosBaseUrl: 'http://sirh-piman.dev.piman2-0.fr/api/public/api/images/',
+  publicPhotosBaseUrl: 'http://sirh-piman.dev.piman2-0.fr/api/public/storage/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
 
-  apiUrl : 'https://xxx/api/',
+  apiUrl : 'http://sirh-piman.dev.piman2-0.fr/api/public/api/',
   appBaseHref : './',
   logo: 'logo-pds.png',
   favicon: 'favicon.ico',
   title: 'PIMAN Group Solutions',
   backgroudImage: 'background.jpg',
-  photosBaseUrl: 'https://xxx/api/images/',
-  publicPhotosBaseUrl: 'https://xxx/api/public/storage/'
+  photosBaseUrl: 'http://sirh-piman.dev.piman2-0.fr/api/public/api/images/',
+  publicPhotosBaseUrl: 'http://sirh-piman.dev.piman2-0.fr/api/public/api/public/storage/'
 };
