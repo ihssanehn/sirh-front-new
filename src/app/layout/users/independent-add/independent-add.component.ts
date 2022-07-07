c4a33920af2a39e0118b7bd444758aca97114adc
anass chbani <Fatima_rahli@hotmail.fr>
2022-07-07 22:22:32 +0100

    add user group add user independent

---
+++ src/app/layout/users/independent-add/independent-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-independent-add',
+  templateUrl: './independent-add.component.html',
+  styleUrls: ['./independent-add.component.scss']
+})
+export class IndependentAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
