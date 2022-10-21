273663d717656500a6bcee77d6f290fc5417cc9c
anass chbani <a.chbani@piman-group.fr>
2022-10-21 11:16:17 +0100

    Module projet: + Création de la page static Création projet => "Lieu d'intervention"

--- src/app/layout/project/creation-lieu-intervention/creation-lieu-intervention.component.ts
+++ src/app/layout/project/creation-lieu-intervention/creation-lieu-intervention.component.ts
@@ -8,6 +8,7 @@ import {MessageService} from "primeng/api";
 import {TranslateService} from "@ngx-translate/core";
 import {ListsService} from "@services/lists.service";
 import {MainStore} from "@store/mainStore.store";
+import {SharedClasses} from "@shared/Utils/SharedClasses";
 
 @Component({
   selector: 'app-creation-lieu-intervention',
@@ -23,7 +24,12 @@ export class CreationLieuInterventionComponent implements OnInit {
   errorLoadData = false;
   loadingData = false;
   formInputs = {
-
+    country_id: 'country_id',
+    calendar_id: 'calendar_id',
+    city_id: 'city_id',
+    postal_code: 'postal_code',
+    address: 'address',
+    mission_description: 'mission_description',
   }
   @Input() title = '';
   @Input() type = '';
@@ -31,6 +37,12 @@ export class CreationLieuInterventionComponent implements OnInit {
   @Input()  submitting: any;
   @Output() next: EventEmitter<any> = new EventEmitter();
   @Output() preview: EventEmitter<any> = new EventEmitter();
+  loadingSelect = {};
+  id_entite;
+  countries = [];
+  calendars = [];
+  cities = [];
+  postalCodes = [];
   constructor(private formBuilder: FormBuilder,
               private errorService: ErrorService,
               private router: Router,
@@ -43,7 +55,13 @@ export class CreationLieuInterventionComponent implements OnInit {
               private listService: ListsService,
               private mainStore: MainStore) {
     this.formGroup = this.formBuilder.group({
-      id: [null]
+      id: [null],
+      country_id: [null],
+      calendar_id: [null],
+      city_id: [null],
+      postal_code: [null],
+      address: [null],
+      mission_description: [null]
     });
   }
 
@@ -61,5 +79,32 @@ export class CreationLieuInterventionComponent implements OnInit {
       this.preview.emit();
     }
   }
+
+  isRequired(control) {
+    return SharedClasses.isControlRequired(this.formGroup.controls[control]) ? '(*)': '';
+  }
+
+  async getFilterList(items, list_name, list_param?){
+    if(items === 'personals'){
+      try{
+        this.loadingSelect[list_name] = true;
+        this[items] = await this.listService.getPersonalsByCpId({entity_id: this.id_entite}).toPromise();
+      } catch (e) {
+        console.log('error filter', e);
+      } finally {
+        this.loadingSelect[list_name] = false;
+      }
+    }else{
+      try{
+        this.loadingSelect[list_name] = true;
+        this[items] = await this.listService.getAll(list_name, list_param).toPromise();
+      } catch (e) {
+        console.log('error filter', e);
+      } finally {
+        this.loadingSelect[list_name] = false;
+      }
+    }
+  }
+
 
 }
