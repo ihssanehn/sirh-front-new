2b8caf8c0e20e15aa15a639de688efec5074a5d1
anass chbani <a.chbani@piman-group.fr>
2022-10-24 12:30:10 +0100

    Module projet: + Création de la page static Création projet => "Pièces jointes"

--- src/app/layout/project/creation-general/creation-general.component.ts
+++ src/app/layout/project/creation-general/creation-general.component.ts
@@ -26,17 +26,17 @@ export class CreationGeneralComponent implements OnInit {
   formInputs = {
     start_date: 'start_date',
     end_date: 'end_date',
-    nbr_initial_days: 'nbr_initial_days',
-    nbr_remaining_days: 'nbr_remaining_days',
+    initial_number_of_days: 'initial_number_of_days',
+    left_number_of_days: 'left_number_of_days',
     consultant_id: 'consultant_id',
-    short_title: 'short_title',
-    tariff_currency: 'tariff_currency',
-    mission_active: 'mission_active',
-    mail_to_manager: 'mail_to_manager',
-    estimated_end: 'estimated_end',
+    short_mission_title: 'short_mission_title',
+    devise_id: 'devise_id',
+    is_active: 'is_active',
+    has_mail_to_manager: 'has_mail_to_manager',
+    end_estimated_date: 'end_estimated_date',
     mission_title: 'mission_title',
     tariff: 'tariff',
-    cp: 'cp',
+    cp_id: 'cp_id'
   }
   @Input() title = '';
   @Input() type = '';
@@ -64,17 +64,17 @@ export class CreationGeneralComponent implements OnInit {
       id: [null],
       start_date: [null],
       end_date: [null],
-      nbr_initial_days: [null],
-      nbr_remaining_days: [null],
+      initial_number_of_days: [null],
+      left_number_of_days: [null],
       consultant_id: [null],
-      short_title: [null],
-      tariff_currency: [null],
-      mission_active: [null],
-      mail_to_manager: [null],
-      estimated_end: [null],
+      short_mission_title: [null],
+      devise_id: [null],
+      is_active: [null],
+      has_mail_to_manager: [null],
+      end_estimated_date: [null],
       mission_title: [null],
       tariff: [null],
-      cp: [null]
+      cp_id: [null]
     });
     this.id_entite = this.mainStore.selectedEntities?.length === 1 ? this.mainStore.selectedEntities[0].id: null;
   }
--- src/app/layout/project/creation-pieces-jointes/creation-pieces-jointes.component.ts
+++ src/app/layout/project/creation-pieces-jointes/creation-pieces-jointes.component.ts
@@ -8,6 +8,9 @@ import {MessageService} from "primeng/api";
 import {TranslateService} from "@ngx-translate/core";
 import {ListsService} from "@services/lists.service";
 import {MainStore} from "@store/mainStore.store";
+import {FileSystemFileEntry, NgxFileDropEntry} from "ngx-file-drop";
+import {SharedClasses} from "@shared/Utils/SharedClasses";
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-creation-pieces-jointes',
@@ -31,6 +34,16 @@ export class CreationPiecesJointesComponent implements OnInit {
   @Input()  submitting: boolean;
   @Output() next: EventEmitter<any> = new EventEmitter();
   @Output() preview: EventEmitter<any> = new EventEmitter();
+  show_loader: boolean;
+  forbiddenExtesionsErrorMessage: string;
+  filesSizeErrorMessage: string;
+  emptyFilesErrorMessage: string;
+  inputFile: any;
+  files = [];
+  edittingMode;
+  dropping: boolean;
+  blackListesExtensions = ['exe', 'com', 'dll', 'bat', 'sh'];
+  ALL_FILES_SIZE_LIMIT = 10000; // Mb
   constructor(private formBuilder: FormBuilder,
               private errorService: ErrorService,
               private router: Router,
@@ -62,4 +75,185 @@ export class CreationPiecesJointesComponent implements OnInit {
     }
   }
 
+  uploadFile(e){
+    console.log('uploadFile', e);
+    this.show_loader = true;
+    // this.component.files = [];
+    if(e.target.files && e.target.files.length>0){
+      if(!this.edittingMode){
+        Array.prototype.forEach.call(e.target.files, file=>{
+          if(!this.findFile(file)){
+            this.files.push(file);
+          }
+        });
+      }else{
+        this.files = [e.target.files[0]];
+      }
+    }
+    this.show_loader = false;
+    if( this.inputFile ){
+      this.inputFile.nativeElement.value='';
+    }
+    console.log('this.component.files', this.files);
+    if(this.files.length>0){
+      // this.error.files = '';
+      this.emptyFilesErrorMessage = '';
+    }else{
+      // this.error.files = 'Vous devez charger un docuemnt';
+      this.emptyFilesErrorMessage = 'Vous devez charger un docuemnt';
+    }
+
+    if(this.getAllFilesSize() > this.ALL_FILES_SIZE_LIMIT){
+      this.filesSizeErrorMessage = 'Vous avez dépassé la taille limite d\'importation de fichiers ('+this.ALL_FILES_SIZE_LIMIT+' Mb)';
+    }else{
+      this.filesSizeErrorMessage = '';
+    }
+
+    const forbiddenExtesions = this.checkExtensions();
+    if(forbiddenExtesions && forbiddenExtesions.length > 0){
+      this.forbiddenExtesionsErrorMessage = forbiddenExtesions.length === 1 ?
+        'L\'extension '+forbiddenExtesions[0]+' n\'est pas prise en charge':
+        'Les extensions ('+forbiddenExtesions.join(', ')+') ne sont pas prises en charge';
+    }else{
+      this.forbiddenExtesionsErrorMessage = '';
+    }
+  }
+
+  async getFile(fileEntry) {
+    try {
+      return await new Promise((resolve, reject) => fileEntry.file(resolve, reject));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async dropFile(files: NgxFileDropEntry[]){
+    this.files = [];
+    if(!this.edittingMode){
+      this.dropping = false;
+      this.show_loader = true;
+      for (const droppedFile of files) {
+        if (droppedFile.fileEntry.isFile) {
+          const fileEntry = await droppedFile.fileEntry as FileSystemFileEntry;
+          const file = await this.getFile(fileEntry);
+          if(!this.findFile(file)){
+            this.files.push(file);
+          }
+        }
+      }
+      this.show_loader = false;
+    }
+
+    if(this.getAllFilesSize() > this.ALL_FILES_SIZE_LIMIT){
+      this.filesSizeErrorMessage = 'Vous avez dépassé la taille limite d\'importation de fichiers ('+this.ALL_FILES_SIZE_LIMIT+' Mb)';
+    }else{
+      this.filesSizeErrorMessage = '';
+    }
+
+    console.log('befot checking extensions', this.files);
+    const forbiddenExtesions = this.checkExtensions();
+    if(forbiddenExtesions && forbiddenExtesions.length > 0){
+      this.forbiddenExtesionsErrorMessage = forbiddenExtesions.length === 1 ?
+        'L\'extension '+forbiddenExtesions[0]+' n\'est pas prise en charge':
+        'Les extensions ('+forbiddenExtesions.join(', ')+') ne sont pas prises en charge';
+    }else{
+      this.forbiddenExtesionsErrorMessage = '';
+    }
+    console.log('forbiddenExtesions', forbiddenExtesions);
+  }
+
+  checkExtensions(){
+    const foundedExtensions = [];
+    console.log('checkExtensions', this.files);
+    this.files.forEach(file => {
+      const ext = file.name.split('.').pop();
+      if(this.blackListesExtensions.includes(ext)){
+        foundedExtensions.push(ext);
+      }
+    });
+    return foundedExtensions;
+  }
+
+  getAllFilesSize(): number{
+    let globalSize = 0;
+    this.files.forEach(file => {
+      globalSize += file.size / 1024 / 1024;
+    });
+    return globalSize;
+  }
+
+  findFile(file) {
+    return this.files.find(function(existingFile) {
+      return (
+        existingFile.name         === file.name &&
+        existingFile.lastModified === file.lastModified &&
+        existingFile.size         === file.size &&
+        existingFile.type         === file.type
+      );
+    });
+  }
+
+  removeFile(fileToRemove){
+    // if(fileToRemove instanceof File){
+    this.files = this.files.filter(file => !(
+      fileToRemove.name         === file.name &&
+      fileToRemove.lastModified === file.lastModified &&
+      fileToRemove.size         === file.size &&
+      fileToRemove.type         === file.type &&
+      fileToRemove.id         === file.id
+    ));
+    // }
+
+    if(this.files.length>0){
+      // this.error.files = '';
+      this.emptyFilesErrorMessage = '';
+    }else{
+      this.emptyFilesErrorMessage = 'Vous devez charger un docuemnt';
+    }
+
+  }
+
+  getFileName (name) {
+    let file_name = name;
+    const arr_filename = file_name.split('.');
+    const file_ex = arr_filename.pop();
+    file_name = arr_filename.join('.');
+    if ( file_name.length > 7 ) {
+      file_name = file_name.substr(0,5) + '...';
+    }
+    return file_name+'.'+file_ex;
+  }
+
+  getIcon(filename) {
+    const file_ex = filename.split('.').pop();
+    console.log('getIcon', filename, file_ex);
+    return 'icon-file-' + SharedClasses.getFileType(file_ex);
+  }
+
+  async download(file){
+    if(!file.id){
+      return;
+    }
+    const params = {
+      id: file.id
+    };
+
+    try{
+      // const res: any = await this.userService.downloadDocument(params).toPromise();
+      // console.log('res blob', res);
+      // const blob = new Blob([res.body]);
+      // FileSaver.saveAs(blob, file.name);
+    }catch(error){
+      console.log('e', error);
+      this.mainStore.showMessage(`Echec de téléchargement!`, `le document n'a pas pu être téléchargé`, 'error');
+    }
+
+  }
+
+  getFileSize(size){
+    let fileSize = size.toString();
+
+    if(fileSize.length < 7) return `${Math.round(+fileSize/1024).toFixed(2)} kb`
+    return `${(Math.round(+fileSize/1024)/1000).toFixed(2)} MB`
+  }
 }
